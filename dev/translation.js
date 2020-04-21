0e3a4e5a554ad7b8954afabccacb9f20a8764ffd
JiChuChiLun <61679493+JiChuChiLun@users.noreply.github.com>
2020-04-20 19:04:47 -0500

    Update translation.js

--- dev/translation.js
+++ dev/translation.js
@@ -1,9 +1,9 @@
-Translation.addTranslation("Quarry", {ru: "Карьер"});
-Translation.addTranslation("Quarry Casing", {ru: "Корпус карьера"});
-Translation.addTranslation("Quarry Upgrade (Territory)", {ru: "Улучшение карьера (Радиус)"});
-Translation.addTranslation("Quarry Lens (Smelt)", {ru: "Линза (Переплавка)"});
-Translation.addTranslation("Range: ", {ru: "Радиус: "});
-Translation.addTranslation("Exp: ", {ru: "Опыт: "});
-Translation.addTranslation("Incorrect tool", {ru: "Неверный инструмент"});
-Translation.addTranslation("Not enough space", {ru: "Недостаточно места"});
-Translation.addTranslation("Incorrect structure", {ru: "Неверное строение"});
+Translation.addTranslation("Quarry", {ru: "Карьер", zh: "采石场"});
+Translation.addTranslation("Quarry Casing", {ru: "Корпус карьера", zh: "采石场框架"});
+Translation.addTranslation("Quarry Upgrade (Territory)", {ru: "Улучшение карьера (Радиус)", zh: "采石场升级(范围)"});
+Translation.addTranslation("Quarry Lens (Smelt)", {ru: "Линза (Переплавка)", zh: "采石场接口(冶炼)"});
+Translation.addTranslation("Range: " {ru: "Радиус: ", zh: "范围: "});
+Translation.addTranslation("Exp: ", {ru: "Опыт: ", zh: "经验"});
+Translation.addTranslation("Incorrect tool", {ru: "Неверный инструмент", zh: "错误的工具"});
+Translation.addTranslation("Not enough space", {ru: "Недостаточно места", zh: "范围大小不足"});
+Translation.addTranslation("Incorrect structure", {ru: "Неверное строение", zh: "错误的结构"});

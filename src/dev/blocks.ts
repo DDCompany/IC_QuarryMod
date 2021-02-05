12bbd56e3f6813620e45e617157e3721c4b08742
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2021-02-05 10:53:43 +0300

    Block.setPrototype -> createBlock

--- src/dev/blocks.ts
+++ src/dev/blocks.ts
@@ -1,27 +1,7 @@
-Block.setPrototype("quarry", {
-    type: Block.TYPE_BASE,
-
-    getVariations: function () {
-        return [{
-            name: "Quarry",
-            texture: [["quarry", 0]],
-            inCreative: true
-        }];
-    }
-
-});
+IDRegistry.genBlockID("quarry");
+Block.createBlock("quarry", [{name: "Quarry", texture: [["quarry", 0]], inCreative: true}]);
 Block.setBlockMaterial(BlockID.quarry, "stone", 2);
 
-Block.setPrototype("quarryCasing", {
-    type: Block.TYPE_BASE,
-
-    getVariations: function () {
-        return [{
-            name: "Quarry Casing",
-            texture: [["quarry", 1]],
-            inCreative: true
-        }];
-    }
-
-});
+IDRegistry.genBlockID("quarryCasing");
+Block.createBlock("quarryCasing", [{name: "Quarry Casing", texture: [["quarry", 1]], inCreative: true}]);
 Block.setBlockMaterial(BlockID.quarryCasing, "stone", 2);

677f0b9c548ccd48251adc807d17f2954ce77020
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2021-03-07 19:22:36 +0300

    Updated copyright

--- src/dev/header.ts
+++ src/dev/header.ts
@@ -1,11 +1,21 @@
 /*
-     ______     __  __     ______     ______     ______     __  __     __    __     ______     _____
-    /\  __ \   /\ \/\ \   /\  __ \   /\  == \   /\  == \   /\ \_\ \   /\ "-./  \   /\  __ \   /\  __-.
-    \ \ \/\_\  \ \ \_\ \  \ \  __ \  \ \  __<   \ \  __<   \ \____ \  \ \ \-./\ \  \ \ \/\ \  \ \ \/\ \
-     \ \___\_\  \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_\ \_\  \/\_____\  \ \_\ \ \_\  \ \_____\  \ \____-
-      \/___/_/   \/_____/   \/_/\/_/   \/_/ /_/   \/_/ /_/   \/_____/   \/_/  \/_/   \/_____/   \/____/
-
-      QuarryMod by Dmitriy Medvedev for ModChallenge3
+ *    ___                              __  __           _ ____  _____
+ *   / _ \ _   _  __ _ _ __ _ __ _   _|  \/  | ___   __| |  _ \| ____|
+ *  | | | | | | |/ _` | '__| '__| | | | |\/| |/ _ \ / _` | |_) |  _|
+ *  | |_| | |_| | (_| | |  | |  | |_| | |  | | (_) | (_| |  __/| |___
+ *   \__\_\\__,_|\__,_|_|  |_|   \__, |_|  |_|\___/ \__,_|_|   |_____|
+ *                               |___/
+ *
+ * Terms of use:
+ *  - Forbidden to distribute the library on third-party sources
+ *    without links to the official group (https://vk.com/forestry_pe)
+ *  - Forbidden to change the code of this mod
+ *  - Forbidden to explicitly copy the code to other libraries or mods
+ *  - Allowed to use in any mod packs
+ *  - Using the mod you automatically agree to the conditions described above
+ *
+ * Textures Author: https://vk.com/vasya.blyaa
+ * @DDCompany (https://vk.com/forestry_pe)
  */
 
 IMPORT("EnergyNet");
